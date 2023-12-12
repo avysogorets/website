745cddf4f854dd77fef0457bb31b72c458d427ce
Artem Vysogorets <avysogorets@a-10-27-9-183.dynapool.vpn.nyu.edu>
2023-12-11 19:44:19 -0500

    initial commit

--- utils.js
+++ utils.js
@@ -3,11 +3,29 @@ button.addEventListener('click', () => {
     const element = document.querySelector('.menu-dropdown');
     element.classList.toggle('visible');
     if (element.classList.contains('visible')) {
-            element.style.maxHeight = element.scrollHeight + "px";
+        button.style.width = 150 + "px";
+        element.style.maxHeight = element.scrollHeight + "px";
     } else {
             element.style.maxHeight = null;
+            setTimeout(closeMenu, 500);
     }    
 });
+
+window.addEventListener('click', function(e){
+    const element = document.querySelector('.menu-dropdown')
+    const button = document.querySelector('.menu-icon');
+    if (!document.querySelector('.menu-icon').contains(e.target)){
+        element.style.maxHeight = null;
+        if (element.classList.contains('visible')) {
+            element.classList.toggle('visible');
+            setTimeout(closeMenu, 500);} 
+    }
+})
+
+function closeMenu() {
+    const button = document.querySelector('.menu-icon');
+    button.style.width = 50 + "px";
+}
 
 function toggleSubcategories(id) {
     var subcategories = document.getElementById(id);
